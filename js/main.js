b0a376b901fdf05ba7cb2c19b0078c327df7b094
AlexanderNP <enin02@mail.ru>
2023-07-16 21:52:26 +0500

    added adaptive for problem, price, team, section

--- js/main.js
+++ js/main.js
@@ -15,7 +15,24 @@ $(function(){
     slidesToShow: 3,
     slidesToScroll: 3,
     speed: 800,
-    appendDots: $('.team__dots-list')
+    appendDots: $('.team__dots-list'),
+    responsive: [
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots:false
+        }
+      },
+    ]
   });
 
   $('.team__slider-prev').on('click', function(e){
